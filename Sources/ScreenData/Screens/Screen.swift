c5ee994e1be64f0b3c306ad87d62c398c7e1d071
Zach Eriksen <zeriksen@fnoex.com>
2020-11-13 16:37:11 -0600

    Added public init for Screen

--- Sources/ScreenData/Screens/Screen.swift
+++ Sources/ScreenData/Screens/Screen.swift
@@ -9,4 +9,22 @@ public struct Screen: Codable {
     public var headerView: SomeView?
     public var view: SomeView
     public var footerView: SomeView?
+    
+    public init(
+        id: String? = nil,
+        title: String,
+        subtitle: String?,
+        backgroundColor: Color,
+        headerView: SomeView? = nil,
+        view: SomeView,
+        footerView: SomeView? = nil
+    ) {
+        self.id = id
+        self.title = title
+        self.subtitle = subtitle
+        self.backgroundColor = backgroundColor
+        self.headerView = headerView
+        self.view = view
+        self.footerView = footerView
+    }
 }
