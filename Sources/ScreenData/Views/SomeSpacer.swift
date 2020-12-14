ac1acd4458a4e75ca9f4811e7596abb61ee5f584
0xLeif <zmeriksen@icloud.com>
2020-12-13 19:19:46 -0600

    Updated Spacer

--- Sources/ScreenData/Views/SomeSpacer.swift
+++ Sources/ScreenData/Views/SomeSpacer.swift
@@ -1,7 +1,12 @@
 public struct SomeSpacer: Codable, Hashable {
+    public var size: Int
     
     public var type: ViewType {
         .spacer
     }
     
+    public init(size: Int) {
+        self.size = size
+    }
 }
+
