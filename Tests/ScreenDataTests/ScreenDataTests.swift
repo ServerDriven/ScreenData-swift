1505efa8b280f2163200757947cf2bbc04115cfc
0xLeif <zmeriksen@icloud.com>
2021-02-02 18:06:15 -0600

    Updated tests

--- Tests/ScreenDataTests/ScreenDataTests.swift
+++ Tests/ScreenDataTests/ScreenDataTests.swift
@@ -6,12 +6,15 @@ final class ScreenDataTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SomeLabel(title: "Hello, World!",
-                                 subtitle: nil,
-                                 style: nil,
-                                 destination: nil).title, "Hello, World!")
+        XCTAssertEqual(
+            SomeLabel(
+                title: "Hello, World!",
+                font: .title
+            ).title,
+            "Hello, World!"
+        )
     }
-
+    
     static var allTests = [
         ("testExample", testExample),
     ]
